3b04c0a2a85836783dcc9dbbeb20e6770d7df617
Iker <83455189+ilobop@users.noreply.github.com>
2022-05-21 17:39:13 +0100

    Create ejemploFor.java
    Repetición en pantalla de un valor en número de veces en Java

---
+++ ejemploFor.java
@@ -0,0 +1,14 @@
+/**
+ *
+ * @author Iker Lobo
+ */
+public class ejemploFor {
+//Repetiremos en pantalla el valor de un número 8 veces
+    public static void main(String args[]) {
+//El valor del número se repetirá 8 veces, es decir de 0 a 7
+       for(int numero=0; numero < 8; numero++){
+//Impresión en pantalla de la repetición
+ System.out.println("El valor de número es:"+numero);
+        }
+    }
+}
